10fb252a97857680905b7a5e02c20d3ed40e14e3
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-06-27 15:54:16 +0530

    initial commit

---
+++ Arrays/merginArray.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void main()
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int a[5] = {2,4,6,18,32};
+    int b[5] = {14,16,20,22,26};
+    int c[10];
+    while(i < 5 && j < 5)
+    {
+        if(a[i] < b[j])
+        {
+            c[k++] = a[i++];
+        }
+        else{
+            c[k++] = b[j++];
+        }
+
+        // for remaining last element
+        for(; i < 5; i++)
+        {
+            c[k++] = a[i];
+        }
+
+         for(; j < 5; j++)
+        {
+            c[k++] = b[j];
+        }
+
+        for(int k = 0; k < 10; k++)
+    {
+        printf("%d ",c[k]);
+    }
+    }
+}
