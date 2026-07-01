c7c4d86b6d5634c0c64d0603c163e362dfeafaf7
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-01 20:13:30 +0530

    initial commit

--- Strings/stringLen.c
+++ Strings/stringLen.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 
+// User define fnx of strlen
+int myLen(char *T)
+{
+    int len = 0;
+    while(*T != '\0')
+    {
+        T++;
+        len++;
+    }
+    return len;
+}
+
 void main()
 {
     char a[] = "Max";
+    char T[] = "Maximus";
     printf("%d ",strlen(a));
+    printf("%d ",myLen(T));
 }
