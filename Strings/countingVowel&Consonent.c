06f19972b675b68373ea2673f6b671cf38885da1
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-03 20:13:33 +0530

    initial commit

---
+++ Strings/countingVowel&Consonent.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i, cconst = 0, vowel = 0;
+    char a[] = "Hi Pool how are you";
+    for(i = 0; i < a[i] != '\0'; i++)
+    {
+        if(a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || 
+        a[i] == 'o' || a[i] == 'u')
+        {
+            vowel++;
+        }
+        else if((a[i] >= 65 && a[i] <= 90) ||(a[i] >= 97 && a[i] <= 122))
+        {
+            cconst++;
+        }
+    }
+    printf("%d ",vowel);
+    printf("%d ",vowel);
+}
