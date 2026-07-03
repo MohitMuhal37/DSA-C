ad3dfc4bcb5006477cb228246efd256c10c5ae8b
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-03 19:56:45 +0530

    initial commit

--- Strings/changingCase.c
+++ Strings/changingCase.c
@@ -3,8 +3,9 @@ void main()
 {
     int i;
     char a[] = "MOHIT MUHAL";
-    for(i = 0; i < a[i] != '\0'; i++)
+    for(i = 0; i < a[i] != '\0' && a[i] == ' '; i++)
     {
+            a[i] = ' ';
         a[i] += 32;
     }
     printf(a);
