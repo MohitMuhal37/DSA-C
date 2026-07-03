fe417c2155b4d3907a4ea8106606afd2ccf8708a
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-03 18:59:13 +0530

    initial commit

---
+++ Strings/findLength.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i;
+    char s[] = "Mohit Muhal";
+    for(int i = 0; i < s[i] != '\0'; i++)
+    {
+
+    }
+    printf("Length of String is %d ",i);
+    return 0;
+}
