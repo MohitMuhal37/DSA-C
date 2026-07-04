856f3e0aa9604af7763f0a478367074ad6f50ba1
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-04 14:01:30 +0530

    initial commit

--- test.c
+++ test.c
@@ -12,19 +12,12 @@ void gfg(int x)
 
 int main()
 {
-    int length = 6;
-    int j, i;
-    int A[6] = {10,20,30,40,50,60};
-    int B[6] = {1,2,3,4,5,6};
-     for( i = length-1 , j = 0 ; i >=0 ; i-- , j++ )
-	 	 {
-	 	 	 B[ j ] = A[ i ];
-             printf("%d ",B[j]);
-	 	 }
-         printf("\n");
-	 	 for( i = 0 ; i < length ; i++ )
-	 	 {
-	 	 	 A[ i ] = B[ i ];
-            //  printf("%d ",A[i]);
-         }
+    long long a = 0;
+    for(long long i = 0 ; i < 100000000000000; i++)
+    {
+      a = a + i;
+    }
+    printf("%lld ", a);
+    // printf("hello");
+    return 0;
 }
