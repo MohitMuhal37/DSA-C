10fdc26f33058af1f53853177dbc1eb5dacd0639
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-05 17:58:57 +0530

    initial commit

--- test.c
+++ test.c
@@ -1,23 +1,14 @@
 #include<stdio.h>
 
-void gfg(int x)
+struct abc
 {
-    printf("%d ", x);
-    if(x <= 2)
-    {
-        gfg(x+1);
-    }
-    printf("%d ",x);
-}
+    char a;
+    char b;
+    int c;
+};
 
 int main()
 {
-    long long a = 0;
-    for(long long i = 0 ; i < 100000000000000; i++)
-    {
-      a = a + i;
-    }
-    printf("%lld ", a);
-    // printf("hello");
-    return 0;
+  struct abc var;
+  printf("%d ",sizeof(var));
 }
