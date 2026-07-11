825aa28986957fc7ada5bf0c12cf123b6effbecc
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-11 22:37:16 +0530

    initial commit

--- LinkedList.c/displayLL.c
+++ LinkedList.c/displayLL.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct node
 {
     int data;
     struct node *next;
 }*p = NULL;
-//Now create a global pointer;
-
-
-//creating linledlist
 
 void create(int a[], int n)
 {
@@ -17,32 +14,29 @@ void create(int a[], int n)
     p = (struct node *)malloc(sizeof(struct node));
     p->data = a[0];
     p->next = NULL;
-    l = p;
+    l=p;
 
-    // create other node using for loop
-    for(int i = 1; i < n; i++)
+    for(i = 1; i < n; i++)
     {
-            t = (struct node*)malloc(sizeof(struct node));
-            t->data = a[i];
-            t->next = NULL;
-            l->next = t;
-            l=t;
-
+        t = (struct node *)malloc(sizeof(struct node));
+        t->data = a[i];
+        t->next = NULL;
+        l->next = t;
+        l = t;
     }
 }
 
-void display(struct node *p)
+void disp(struct node *p)
 {
     while(p != NULL)
     {
         printf("%d ",p->data);
-        p=p->next;
+        p = p -> next;
     }
 }
-
 int main()
 {
-    int a[] = {3,4,5,6,7,87};
+    int a[] ={10,20,30,40,50};
     create(a,6);
-    display(p);
+    disp(p);
 }
