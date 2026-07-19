2117aee4ec348842a1e26d7d0d5d810b98d0b4e8
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-07-19 18:34:41 +0530

    initial commit

--- LinkedList.c/maxLL.c
+++ LinkedList.c/maxLL.c
@@ -16,7 +16,7 @@ void create(int a[], int n)
     p->next = NULL;
     last  = p;
 
-    for(i = 0; i < n; i++)
+    for(i = 1; i < n; i++)
     {
          t = (struct node *) malloc(sizeof(struct node));
          t->data = a[i];
@@ -35,9 +35,25 @@ void show(struct node *p)
     }
 }
 
+// find maximum in list
+int maxLL(struct node *p)
+{
+        int m = -32568;
+        while(p)
+        {
+            if(p->data >= m )
+            {
+                m = p->data;
+            }
+            p=p->next;
+        }  
+        return m; 
+}
+
 int main()
 {
     int a[] = {10,30,2,45,6};
     create(a,5);
     show(p);
+    printf("%d \n",maxLL(p));
 }
